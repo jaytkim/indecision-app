0f4dbe556e5bba8ac16a1bdd6f9386d96f9aa40f
Thomas Kim <jaytkim@gmail.com>
2018-02-07 18:05:13 -0500

    es6-arrow-function

--- src/playground/es6-arrow-functions.js
+++ src/playground/es6-arrow-functions.js
@@ -1,5 +1,3 @@
-'use strict';
-
 // const square = function (x) {
 //     return x * x;
 // };
@@ -23,8 +21,6 @@
 //     return fullName.split(' ')[0];    
 // };
 
-var getFirstName = function getFirstName(fullName) {
-  return fullName.split(' ')[0];
-};
+const getFirstName = (fullName) => fullName.split(' ')[0];
 
 console.log(getFirstName('Mike Smith'));
